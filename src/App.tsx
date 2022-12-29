6f113bfc2e5ae8522bb137ce853de520791f1f79
blessedmadukoma <blessedmadukoma@gmail.com>
2022-12-29 14:13:16 +0100

    added TypeScript generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Counter from "./components/Counter";
 import Heading from "./components/Heading";
+import List from "./components/List";
 import Section from "./components/Section";
 
 function App() {
@@ -8,6 +9,10 @@ function App() {
       <Heading title="Hello" />
       <Section title="Sub heading">This is my section</Section>
       <Counter />
+      <List
+        items={["Coffie", "Tacos", "Sugar"]}
+        render={(item: string) => <span className="bold gold">{item}</span>}
+      />
     </>
   );
 }
